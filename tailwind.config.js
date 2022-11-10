2006d35167a4868e1131dcc8bec7e9204c1c3537
harismauladi <haris1922.hmh@gmail.com>
2022-11-10 11:55:33 +0700

    protofolio-web

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    fontFamily:{
+    'Poppins' : 'Poppins',
+    'sec' : 'Josefin Sans'
+    },
+    extend: {
+      colors : {
+        'primary' : '#0F172A',
+        'secondary' : '#10F8B2'
+      },
+      animation : {
+        'slide' : 'slide 12s infinite'
+      },
+      keyframes : {
+        'slide' : {
+          '0%'  : {'margin-top' : '0px'},
+          '100%' : {'margin-top' : '0px'},
+          '16%' : {'margin-top' : '-60px'},
+          '82%' : {'margin-top' : '-60px'},
+          '33%' : {'margin-top' : '-120px'},
+          '66%' : {'margin-top' : '-120px'},
+          '50%' : {'margin-top' : '-150px'}
+        }
+      },
+      lineHeight : {
+        'leading-11' : '54px'
+      }
+
+    },
+  },
+  plugins: [],
+}
