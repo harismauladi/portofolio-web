246928a63123f77545117338591d95e7ac60bc66
Haris Mauladi <haris1922.hmh@gmail.com>
2023-07-27 20:56:51 +0700

    Add new UI

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const navMenu = document.getElementById("navMenu");
+const navList = document.getElementById("nav-list");
+const submitBtn = document.getElementById("submitBtn");
+
+navMenu.addEventListener("click", function () {
+  navMenu.classList.toggle("nav-active");
+  navList.classList.toggle("hidden");
+});
+const navTitle = document.getElementById("nav-title");
+const span1 = document.getElementById("span1");
+const span2 = document.getElementById("span2");
+const span3 = document.getElementById("span3");
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const navFixed = header.offsetTop;
+
+  if (window.pageYOffset > navFixed) {
+    header.classList.add("nav-fixed");
+    navTitle.classList.remove("text-white");
+    navTitle.classList.add("text-primary");
+    span1.classList.remove("bg-white");
+    span1.classList.add("bg-slate-900");
+    span2.classList.remove("bg-white");
+    span2.classList.add("bg-slate-900");
+    span3.classList.remove("bg-white");
+    span3.classList.add("bg-slate-900");
+    link1.classList.remove("lg:text-white");
+    link1.classList.add("lg:text-primary");
+    link2.classList.remove("lg:text-white");
+    link2.classList.add("lg:text-primary");
+    link3.classList.remove("lg:text-white");
+    link3.classList.add("lg:text-primary");
+  } else {
+    header.classList.remove("nav-fixed");
+    navTitle.classList.add("text-white");
+    navTitle.classList.remove("text-primary");
+    span1.classList.add("bg-white");
+    span1.classList.remove("bg-slate-900");
+    span2.classList.add("bg-white");
+    span2.classList.remove("bg-slate-900");
+    span3.classList.add("bg-white");
+    span3.classList.remove("bg-slate-900");
+    link1.classList.add("lg:text-white");
+    link1.classList.remove("lg:text-primary");
+    link2.classList.add("lg:text-white");
+    link2.classList.remove("lg:text-primary");
+    link3.classList.add("lg:text-white");
+    link3.classList.remove("lg:text-primary");
+  }
+};
+
+submitBtn.addEventListener("click", function () {
+  Swal.fire({
+    icon: "success",
+    title: "Email has been sent",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
-    fontFamily:{
-    'Poppins' : 'Poppins',
-    'sec' : 'Josefin Sans'
+    fontFamily: {
+      Poppins: "Poppins",
+      sec: "Josefin Sans",
     },
     extend: {
-      colors : {
-        'primary' : '#0F172A',
-        'secondary' : '#10F8B2'
+      colors: {
+        primary: "#0F172A",
+        secondary: "#10F8B2",
       },
-      animation : {
-        'slide' : 'slide 12s infinite'
+      animation: {
+        slide: "slide 12s infinite",
       },
-      keyframes : {
-        'slide' : {
-          '0%'  : {'margin-top' : '0px'},
-          '100%' : {'margin-top' : '0px'},
-          '16%' : {'margin-top' : '-60px'},
-          '82%' : {'margin-top' : '-60px'},
-          '33%' : {'margin-top' : '-120px'},
-          '66%' : {'margin-top' : '-120px'},
-          '50%' : {'margin-top' : '-150px'}
-        }
+      keyframes: {
+        slide: {
+          "0%": { "margin-top": "0px" },
+          "100%": { "margin-top": "0px" },
+          "16%": { "margin-top": "-60px" },
+          "82%": { "margin-top": "-60px" },
+          "33%": { "margin-top": "-120px" },
+          "66%": { "margin-top": "-120px" },
+          "50%": { "margin-top": "-150px" },
+        },
       },
-      lineHeight : {
-        'leading-11' : '54px'
-      }
-
+      lineHeight: {
+        "leading-11": "54px",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
